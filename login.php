32c373328a346d547bbf37bd101ec7f6170406f7
xyz-cmyk <kousik.sengupta@ymail.com>
2020-06-18 17:50:13 +0530

    new

---
+++ login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Login page</title>
+</head>
+<body>
+  <div class="header">
+    <h2>Login</h2>
+   </div>
+   <form method="post" action="login.php">
+    <div class="input">
+    <label>Username</label>
+    <input type="text" name="username">
+    </div>
+    
+    <div class="input">
+    <label>Password</label>
+    <input type="password" name="password">
+    <div class="input">
+    <button type="submit" name="login" class="btn">Login</button>
+    </div>
+    </div>
+    <p>Not yet a member? <a href="registration.php">Sign up</a>
+</p>
+    </form>
+    </body>
+    </html> 
---
+++ registration.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Login page</title>
+</head>
+<body>
+  <div class="header">
+    <h2>Register Now</h2>
+   </div>
+   <form method="post" action="registration.php">
+    <div class="input">
+    <label>Username</label>
+    <input type="text" name="username">
+    </div>
+    <div class="input">
+    <label>Email</label>
+    <input type="email" name="email">
+    </div>
+    <div class="input">
+    <label>Password</label>
+    <input type="password" name="password">
+    <div class="input">
+    <button type="submit" name="register" class="btn">Register</button>
+    </div>
+    </div>
+    <p>Already a member? <a href="login.php">Sign in</a>
+</p>
+    </form>
+    </body>
+    </html> 
