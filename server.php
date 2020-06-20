2c9ed81220bc6c88ff023f493613ddc8943f23ed
xyz-cmyk <kousik.sengupta@ymail.com>
2020-06-20 10:42:16 +0530

    updated

--- server.php
+++ server.php
@@ -7,26 +7,26 @@ $errors = array();
 
 $db = mysqli_connect('localhost', 'root', '', 'diary');//here diary is my database name
 if(isset($_POST['register'])){
-    $username=mysql_real_escape_string($_POST['username']);
-    $email=mysql_real_escape_string($_POST['email']);
-    $password=mysql_real_escape_string($_POST['password']);
+   $username=mysqli_real_escape_string($db, $_POST['username']);
+   $email=mysqli_real_escape_string($db, $_POST['email']);
+   $password=mysqli_real_escape_string($db, $_POST['password']);
 
-    if (empty($username)){
+   if (empty($username)){
         array_push($errors, "Username is required");
     }
-    if (empty($email)){
+   if (empty($email)){
             array_push($errors, "Email is required");
-    }    
-    if (empty($password)){
+   }    
+   if (empty($password)){
                 array_push($errors, "Password is required");
-    }
-    if (count($errors)==0){ //if no error found, save to database
+   }
+   if (count($errors)==0){ //if no error found, save to database
         $password = md5($password);
         $sql = "INSERT INTO user (username, email, password) 
                    VALUES ('$username', '$email', '$password')";
     
         mysqli_query($db, $sql);
-    }
+   }
 
 }
 ?>
